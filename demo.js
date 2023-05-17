e1d673dcb2b7361c255c6a4d6e5f7090710ea895
Alex <aferrara3@gatech.edu>
2023-05-17 18:20:13 -0400

    cpi updates

--- demo.js
+++ demo.js
@@ -1,16 +1,30 @@
+var x_dat
+var y_dat
+var wait = true
+
 d3.csv("https://alexanderferrara.com/files/CPI_out.csv", function(err, rows){
 
   function unpack(rows, key) {
   return rows.map(function(row) { return row[key]; });
 }
 
+x_dat = unpack(rows, 'Date')
+y_dat = unpack(rows, 'Value')
+
+wait = false
+
+});
+
+while(wait){
+	await new Promise(r => setTimeout(r, 500));
+}
 
 var trace1 = {
   type: "scatter",
   mode: "lines",
   name: 'Buying Power',
-  x: unpack(rows, 'Date'),
-  y: unpack(rows, 'Value'),
+  x: x_dat,
+  y: y_dat.map(x => x * 1),
   line: {color: '#17BECF'}
 }
 
@@ -21,4 +35,3 @@ var layout = {
 };
 
 Plotly.newPlot('myDiv', data, layout);
-})
